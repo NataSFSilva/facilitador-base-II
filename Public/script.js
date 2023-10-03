71b9024691b39deea260a5d1880db491c98b0322
NataSFSilva <nata.ssilva@sptech.school>
2023-10-03 10:20:32 -0300

    Esboço inicial

---
+++ Public/script.js
@@ -0,0 +1,65 @@
+var linhas = [
+    {
+        emissor: "Ambev",
+        arquivo: "",
+        tamanhoArquivo: "",
+        received: "",
+        processed: ""
+    },
+    {
+        emissor: "BBNK",
+        arquivo: "",
+        tamanhoArquivo: "",
+        received: "",
+        processed: ""
+    },    
+    {
+        emissor: "Bene",
+        arquivo: "",
+        tamanhoArquivo: "",
+        received: "",
+        processed: ""
+    },
+    {
+        emissor: "BitCaptal",
+        arquivo: "",
+        tamanhoArquivo: "",
+        received: "",
+        processed: ""
+    },    
+    {
+        emissor: "Caju",
+        arquivo: "",
+        tamanhoArquivo: "",
+        received: "",
+        processed: ""
+    },
+    {
+        emissor: "Mozper",
+        arquivo: "",
+        tamanhoArquivo: "",
+        received: "",
+        processed: ""
+    },    
+    {
+        emissor: "Rapyd",
+        arquivo: "",
+        tamanhoArquivo: "",
+        received: "",
+        processed: ""
+    },
+    {
+        emissor: "Zoop_iFood",
+        arquivo: "",
+        tamanhoArquivo: "",
+        received: "",
+        processed: ""
+    },    
+    {
+        emissor: "ZroBank",
+        arquivo: "",
+        tamanhoArquivo: "",
+        received: "",
+        processed: ""
+    }
+]
