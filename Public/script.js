fb6ab0cce8c66fc50f9d2bab5129dc25e26988c4
NataSFSilva <nata.ssilva@sptech.school>
2023-10-05 14:00:54 -0300

    Primeiro teste

--- Public/script.js
+++ Public/script.js
@@ -12,7 +12,7 @@ var linhas = [
         tamanhoArquivo: "",
         received: "",
         processed: ""
-    },    
+    },
     {
         emissor: "Bene",
         arquivo: "",
@@ -26,7 +26,7 @@ var linhas = [
         tamanhoArquivo: "",
         received: "",
         processed: ""
-    },    
+    },
     {
         emissor: "Caju",
         arquivo: "",
@@ -40,7 +40,7 @@ var linhas = [
         tamanhoArquivo: "",
         received: "",
         processed: ""
-    },    
+    },
     {
         emissor: "Rapyd",
         arquivo: "",
@@ -54,7 +54,7 @@ var linhas = [
         tamanhoArquivo: "",
         received: "",
         processed: ""
-    },    
+    },
     {
         emissor: "ZroBank",
         arquivo: "",
@@ -63,3 +63,45 @@ var linhas = [
         processed: ""
     }
 ]
+
+function dataAtual() {
+    const dataAtual = new Date();
+
+    const dia = String(dataAtual.getDate()).padStart(2, '0');
+
+    const mes = String(dataAtual.getMonth() + 1).padStart(2, '0');
+
+    const dataFormatada = ` - ${dia}/${mes}`;
+    console.log(dataFormatada);
+
+    linhas.forEach(dia => {
+        dia.received += dataFormatada;
+        dia.processed += dataFormatada;
+    });
+}
+
+// function carregarDados() {
+//     linhas.forEach((dado, index) => {
+
+//     });
+// }
+
+function gerarTable() {
+    var texto = (document.getElementById("alertas").value).split("\n");
+    console.log(texto);
+    // var tbl = document.getElementById("tabela");
+    // for (let i = 0; i < linhas.length; i++) {
+    //     var linha = document.createElement("tr");
+    //     tbl.appendChild(linha);
+
+    //     var emissor = document.createElement("td");
+    //     var emissor = document.createElement("td");
+    //     var emissor = document.createElement("td");
+    //     var emissor = document.createElement("td");
+    //     var emissor = document.createElement("td");
+    //     linha.appendChild(emissor);
+
+    //     emissor.innerHTML = `${linhas[i].emissor}`;
+    // }
+    dataAtual()
+}
